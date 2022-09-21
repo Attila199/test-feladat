c368fd187a7566d26064302554c2d79e66d64d1a
Attila199 <113891483+Attila199@users.noreply.github.com>
2022-09-21 13:04:06 +0200

    3.0

---
+++ Test3.0.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Irjon be két egész számot!");
+            Console.WriteLine("Első szám: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Második szám: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("Az  első szám a nagyobb");
+            }
+            else
+            {
+                Console.WriteLine("A második szám a nagyobb");
+            }
+            Console.WriteLine("\nProgram vége");
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
